48091cbf4b6431f65e952c59ce6c9d4cec388c43
Abidul Wahab Ramadan <abidul.rmdn@gmail.com>
2016-12-15 22:22:11 +0800

    processing the queries from golang using the youtube-dl process and golang routines

--- y2search-crawler/src/hello.go
+++ y2search-crawler/src/hello.go
@@ -1,24 +1,58 @@
-/*
-Copyright 2014 Google Inc.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-     http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
-	"fmt"
-
-	"github.com/golang/example/stringutil"
+  "fmt"
+  "time"
+  "log"
 )
+import "os/exec"
+
+func pinger(c chan string) {
+  for i := 0; ; i++ {
+    c <- "ping"
+  }
+}
+
+func printer(c chan string) {
+  for {
+    msg := <- c
+    fmt.Println(msg)
+    commandParams := " --write-auto-sub --skip-download \"https://www.youtube.com/watch?v=QECX7YvzF_c\" -o srts/here_" + time.Now().String() + ".srt"
+    command := "youtube-dl"
+    cmd := exec.Command(command, commandParams)
+	cmd.Run()
+
+	log.Printf("command : %v", commandParams)
+	
+    time.Sleep(time.Second * 1)
+  }
+}
+
+func ponger(c chan string) {
+  for i := 0; ; i++ {
+    c <- "pong"
+  }
+}
 
 func main() {
-	fmt.Println(stringutil.Reverse("!selpmaxe oG ,olleH"))
+  var c chan string = make(chan string)
+
+  go pinger(c)
+  go ponger(c)
+
+  go printer(c)
+  go printer(c)
+  go printer(c)
+  go printer(c)
+  go printer(c)
+  go printer(c)
+  go printer(c)
+  go printer(c)
+  go printer(c)
+  go printer(c)
+  go printer(c)
+  go printer(c)
+
+  var input string
+  fmt.Scanln(&input)
 }
