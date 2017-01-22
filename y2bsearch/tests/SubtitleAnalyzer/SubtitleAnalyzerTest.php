a289694973de926ec794990a9aefaebb285ea11d
Abidul Wahab Ramadan <abidul.rmdn@gmail.com>
2017-01-22 14:59:51 +0800

    Fixing data provider for the test for the auto-deployment #29

--- y2bsearch/tests/SubtitleAnalyzer/SubtitleAnalyzerTest.php
+++ y2bsearch/tests/SubtitleAnalyzer/SubtitleAnalyzerTest.php
@@ -35,6 +35,10 @@ class SubtitleAnalyzerTest extends \PHPUnit_Framework_TestCase
                         "_id" => "n4bucphC9r4",
                         "_score" => 1.756412,
                         "_source" => [
+                            "start" => 12,
+                            "sentence" => ",align:start position:19%\npasta<c.colorCCCCCC><00:00:02.879><c> hot
+</c><00:00:03.330><c>dog</c><00:00:03.720><c> what</c><00:00:03.840><c> better</c><00:00:04.140><c> way</c><00:00:04.290>
+<c> to</c><00:00:04.350><c> end</c></c>",
                             "subtitles" => [],
                             "image_default" => "https://i.ytimg.com/vi/n4bucphC9r4/default.jpg",
                             "image_medium" => "https://i.ytimg.com/vi/n4bucphC9r4/mqdefault.jpg",
@@ -53,7 +57,7 @@ class SubtitleAnalyzerTest extends \PHPUnit_Framework_TestCase
                             "video_hash_id" => "n4bucphC9r4",
                             "language" => "en",
                             "tags" => null,
-                            "video_url" => "https://www.youtube.com/watch?v=n4bucphC9r4",
+                            "video_url" => "https://www.youtube.com/watch?v=n4bucphC9r4&t=2",
                             "@timestamp" => "2017-01-21T09:06:12.516Z",
                             "video_title" => "$27 Cake Vs. $1,120 Cake",
                             "@version" => "1",
