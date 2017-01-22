10ef41b0c301ccb236b89542821e0b9b9d7a21e9
Abidul Wahab Ramadan <abidul.rmdn@gmail.com>
2017-01-22 14:26:23 +0800

    fixing syntax errors

--- y2bsearch/tests/SubtitleAnalyzer/SubtitleAnalyzerTest.php
+++ y2bsearch/tests/SubtitleAnalyzer/SubtitleAnalyzerTest.php
@@ -151,6 +151,7 @@ class SubtitleAnalyzerTest extends \PHPUnit_Framework_TestCase
                         ],
                     ],
                 ],
-            ]
+            ],
+        ];
     }
 }
