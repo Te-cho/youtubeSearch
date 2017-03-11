fe90dbf025035850f863d713d1a9f7639293f6b3
Abidul Wahab Ramadan <abidul.rmdn@gmail.com>
2017-03-11 22:57:41 +0800

    changing the contact us to my linkedin account #45

--- y2bsearch/resources/views/subviews/footer.blade.php
+++ y2bsearch/resources/views/subviews/footer.blade.php
@@ -3,7 +3,7 @@
 		<div class="ui horizontal inverted small divided link list">
 			<img src="{{asset('/images/logo.png')}}" class="ui centered mini image item">
 			<a class="item" href="mailto:abidul.rmdn@gmail.com?Subject=I%20love%20your%20website" target="_top">Email Us</a>
-			<a class="item" href="https://github.com/abdrmdn" target="_blank">Contact Us</a>
+			<a class="item" href="https://www.linkedin.com/in/abdrmdn/" target="_blank">Contact Us</a>
 		</div>
 	</div>
 </div>
