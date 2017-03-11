9d00217b2f507a5ad621b0bac4f11492b7f7f6cf
Abidul Wahab Ramadan <abidul.rmdn@gmail.com>
2017-03-11 22:55:51 +0800

    styling the note msg in the search page about improving the results #45

--- y2bsearch/resources/views/search-page.blade.php
+++ y2bsearch/resources/views/search-page.blade.php
@@ -3,7 +3,7 @@
 @section("content")
     @include('subviews.header', ['showSearch'=> true])
     <div id="content">
-        <div>our library growing, so keep using to expand it</div>
+        <div style="text-align: center;margin-bottom: 20px;">our library growing, so keep using to expand it</div>
         @include('subviews.videos')
     </div>
 @endsection("content")
